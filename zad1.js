2e85dbd61da0257733c7837c64849df80ad3cc65
Annjakam <kaminskaania27@gmail.com>
2023-02-19 18:50:46 +0100

    first commit

--- zad1.js
+++ zad1.js
@@ -1,8 +1,15 @@
 const prompt = require("prompt-sync")({ sigint: true });
-const ilosc = prompt("Stopnie celsjusza ");
+const ilosc1 = prompt("1 liczby całkowite");
+const ilosc2= prompt("2 liczby całkowite")
 // console.log(`You are ${age} years old.`);
 // if (age >=18){console.log(`pełnoletni.`)}
 // else {console.log(`niepełnoletni.`)};
-let x ;
-x= 1.8 * ilosc + 32
-console.log (x)
+console.log (ilosc1, ilosc2);
+if (ilosc1>ilosc2)
+{
+console.log (ilosc2, ilosc1)
+}
+if (ilosc2>ilosc1)
+{
+console.log (ilosc1, ilosc2)
+}
