0fc351a4caed712d63e40ade786efd6e9d139781
Annjakam <kaminskaania27@gmail.com>
2023-02-19 18:21:59 +0100

    first commit

---
+++ zad1.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")({ sigint: true });
+const ilosc = prompt("Ile gwiazdek ");
+// console.log(`You are ${age} years old.`);
+// if (age >=18){console.log(`pełnoletni.`)}
+// else {console.log(`niepełnoletni.`)};
+let s = "" 
+for (let i=0; i<ilosc; i++) {//console.log ('*');
+s += '*';  
+console.log(s)
+//console.log ('*')
+}
